2cdb465f4390f00e0d08e84d664b2043fc2c3c04
lafadzdhiwa <128008313+lafadzdhiwa@users.noreply.github.com>
2023-05-15 09:01:41 +0700

    Add files via upload

---
+++ Praktikum/Praktikum 5/Jabatan/Jabatan/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.submit = New System.Windows.Forms.Button()
+        Me.txtkode = New System.Windows.Forms.TextBox()
+        Me.txtketerangan = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'submit
+        '
+        Me.submit.Location = New System.Drawing.Point(204, 105)
+        Me.submit.Name = "submit"
+        Me.submit.Size = New System.Drawing.Size(114, 32)
+        Me.submit.TabIndex = 0
+        Me.submit.Text = "SUBMIT"
+        Me.submit.UseVisualStyleBackColor = True
+        '
+        'txtkode
+        '
+        Me.txtkode.Location = New System.Drawing.Point(112, 63)
+        Me.txtkode.Name = "txtkode"
+        Me.txtkode.Size = New System.Drawing.Size(206, 22)
+        Me.txtkode.TabIndex = 1
+        '
+        'txtketerangan
+        '
+        Me.txtketerangan.Location = New System.Drawing.Point(112, 162)
+        Me.txtketerangan.Name = "txtketerangan"
+        Me.txtketerangan.Size = New System.Drawing.Size(206, 22)
+        Me.txtketerangan.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 17)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Kode Jabatan"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 162)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Keterangan"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(430, 239)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtketerangan)
+        Me.Controls.Add(Me.txtkode)
+        Me.Controls.Add(Me.submit)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents submit As Button
+    Friend WithEvents txtkode As TextBox
+    Friend WithEvents txtketerangan As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Praktikum/Praktikum 5/Jabatan/Jabatan/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub submit_Click(sender As Object, e As EventArgs) Handles submit.Click
+        Select Case txtkode.Text
+            Case "MGR" : txtketerangan.Text = "Manager"
+
+            Case "SKR" : txtketerangan.Text = "Sekretaris"
+
+            Case "BDH" : txtketerangan.Text = "Bendahara"
+
+            Case Else : txtketerangan.Text = "Staff"
+        End Select
+    End Sub
+End Class
