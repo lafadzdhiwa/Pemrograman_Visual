5e613b0dcd7138b2ee9be7aba8d52493f2a62488
lafadzdhiwa <128008313+lafadzdhiwa@users.noreply.github.com>
2023-05-15 08:53:51 +0700

    Add files via upload

---
+++ Praktikum/Praktikum 3/Kubus/Kubus/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub hitung_Click(sender As Object, e As EventArgs) Handles hitung.Click
+        Dim sisi, luas, volume As Integer
+        sisi = Val(txtsisi.Text)
+
+        volume = sisi ^ 3
+        luas = 6 * (sisi ^ 2)
+
+        txtluas.Text = Str(luas)
+        txtvolume.Text = Str(volume)
+    End Sub
+End Class
