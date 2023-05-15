bbc1cb25650e376546797966c1c352a77eb15014
lafadzdhiwa <128008313+lafadzdhiwa@users.noreply.github.com>
2023-05-15 08:55:35 +0700

    Add files via upload

---
+++ Praktikum/Praktikum 3/Tabung/Tabung/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub hitung_Click(sender As Object, e As EventArgs) Handles hitung.Click
+        Dim jari, tinggi, volume, luas As Integer
+        jari = Val(txtjari.Text)
+        tinggi = Val(txttinggi.Text)
+
+        volume = 3.14 * (jari ^ 2) * tinggi
+        luas = 2 * 3.14 * jari * (jari + tinggi)
+
+        txtluas.Text = Str(luas)
+        txtvolume.Text = Str(volume)
+    End Sub
+End Class
