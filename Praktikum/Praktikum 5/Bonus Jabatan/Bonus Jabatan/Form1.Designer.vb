f415a6e5d65fdba9b1b8ed362f034c3eca6f2c5c
lafadzdhiwa <128008313+lafadzdhiwa@users.noreply.github.com>
2023-05-15 09:00:47 +0700

    Add files via upload

---
+++ Praktikum/Praktikum 5/Bonus Jabatan/Bonus Jabatan/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtjabatan = New System.Windows.Forms.TextBox()
+        Me.txtketerangan = New System.Windows.Forms.TextBox()
+        Me.submit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtjabatan
+        '
+        Me.txtjabatan.Location = New System.Drawing.Point(156, 84)
+        Me.txtjabatan.Name = "txtjabatan"
+        Me.txtjabatan.Size = New System.Drawing.Size(150, 22)
+        Me.txtjabatan.TabIndex = 3
+        '
+        'txtketerangan
+        '
+        Me.txtketerangan.Location = New System.Drawing.Point(156, 165)
+        Me.txtketerangan.Name = "txtketerangan"
+        Me.txtketerangan.Size = New System.Drawing.Size(150, 22)
+        Me.txtketerangan.TabIndex = 4
+        '
+        'submit
+        '
+        Me.submit.Location = New System.Drawing.Point(347, 124)
+        Me.submit.Name = "submit"
+        Me.submit.Size = New System.Drawing.Size(88, 32)
+        Me.submit.TabIndex = 5
+        Me.submit.Text = "SUBMIT"
+        Me.submit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(114, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(230, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Lafadz Dhiwa Singgih (200511045)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(39, 170)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Keterangan"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(39, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Jabatan"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(479, 253)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.submit)
+        Me.Controls.Add(Me.txtketerangan)
+        Me.Controls.Add(Me.txtjabatan)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtjabatan As TextBox
+    Friend WithEvents txtketerangan As TextBox
+    Friend WithEvents submit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Praktikum/Praktikum 5/Bonus Jabatan/Bonus Jabatan/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub submit_Click(sender As Object, e As EventArgs) Handles submit.Click
+        Select Case txtjabatan.Text
+            Case "Manager" : txtketerangan.Text = "Laptop, Flasdisk"
+
+            Case "Bendahara" : txtketerangan.Text = "Buku catatan, Kalkulator"
+
+            Case "HRD" : txtketerangan.Text = "Handphone, Buku Psikologi"
+
+            Case "Enginer" : txtketerangan.Text = "Tool Kit"
+
+            Case Else : txtketerangan.Text = "Maaf Anda Tidak Mendapat Bonus"
+        End Select
+    End Sub
+End Class
