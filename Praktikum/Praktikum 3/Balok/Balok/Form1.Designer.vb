94d450778b110309d16c352def187957ad5a57f5
lafadzdhiwa <128008313+lafadzdhiwa@users.noreply.github.com>
2023-05-15 08:45:35 +0700

    Add files via upload

---
+++ Praktikum/Praktikum 3/Balok/Balok/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.hitung = New System.Windows.Forms.Button()
+        Me.txtpanjang = New System.Windows.Forms.TextBox()
+        Me.txtlebar = New System.Windows.Forms.TextBox()
+        Me.txtvolume = New System.Windows.Forms.TextBox()
+        Me.txtluas = New System.Windows.Forms.TextBox()
+        Me.txttinggi = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'hitung
+        '
+        Me.hitung.Location = New System.Drawing.Point(136, 192)
+        Me.hitung.Name = "hitung"
+        Me.hitung.Size = New System.Drawing.Size(82, 41)
+        Me.hitung.TabIndex = 0
+        Me.hitung.Text = "Hitung"
+        Me.hitung.UseVisualStyleBackColor = True
+        '
+        'txtpanjang
+        '
+        Me.txtpanjang.Location = New System.Drawing.Point(147, 38)
+        Me.txtpanjang.Name = "txtpanjang"
+        Me.txtpanjang.Size = New System.Drawing.Size(100, 22)
+        Me.txtpanjang.TabIndex = 1
+        '
+        'txtlebar
+        '
+        Me.txtlebar.Location = New System.Drawing.Point(147, 76)
+        Me.txtlebar.Name = "txtlebar"
+        Me.txtlebar.Size = New System.Drawing.Size(100, 22)
+        Me.txtlebar.TabIndex = 2
+        '
+        'txtvolume
+        '
+        Me.txtvolume.Location = New System.Drawing.Point(28, 278)
+        Me.txtvolume.Name = "txtvolume"
+        Me.txtvolume.Size = New System.Drawing.Size(100, 22)
+        Me.txtvolume.TabIndex = 3
+        '
+        'txtluas
+        '
+        Me.txtluas.Location = New System.Drawing.Point(226, 278)
+        Me.txtluas.Name = "txtluas"
+        Me.txtluas.Size = New System.Drawing.Size(100, 22)
+        Me.txtluas.TabIndex = 4
+        '
+        'txttinggi
+        '
+        Me.txttinggi.Location = New System.Drawing.Point(147, 116)
+        Me.txttinggi.Name = "txttinggi"
+        Me.txttinggi.Size = New System.Drawing.Size(100, 22)
+        Me.txttinggi.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Panjang"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 79)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Lebar"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 119)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Tinggi"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(58, 249)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 17)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Volume"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(223, 249)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(115, 17)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Luas Permukaan"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(382, 347)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txttinggi)
+        Me.Controls.Add(Me.txtluas)
+        Me.Controls.Add(Me.txtvolume)
+        Me.Controls.Add(Me.txtlebar)
+        Me.Controls.Add(Me.txtpanjang)
+        Me.Controls.Add(Me.hitung)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents hitung As Button
+    Friend WithEvents txtpanjang As TextBox
+    Friend WithEvents txtlebar As TextBox
+    Friend WithEvents txtvolume As TextBox
+    Friend WithEvents txtluas As TextBox
+    Friend WithEvents txttinggi As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Praktikum/Praktikum 3/Balok/Balok/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub hitung_Click(sender As Object, e As EventArgs) Handles hitung.Click
+        Dim panjang, lebar, tinggi, volume, luas As Integer
+        panjang = Val(txtpanjang.Text)
+        lebar = Val(txtlebar.Text)
+        tinggi = Val(txttinggi.Text)
+
+        volume = panjang * lebar * tinggi
+        luas = 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi))
+
+        txtluas.Text = Str(luas)
+        txtvolume.Text = Str(volume)
+    End Sub
+End Class
